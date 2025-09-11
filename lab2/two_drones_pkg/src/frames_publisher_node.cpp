ec64e73957932654260df00e45de021980c4828b
Harshin <me19b109@smail.iitm.ac.in>
2025-09-11 21:20:37 +0530

    Lab2

--- lab2/two_drones_pkg/src/frames_publisher_node.cpp
+++ lab2/two_drones_pkg/src/frames_publisher_node.cpp
@@ -13,14 +13,16 @@ class FramesPublisherNode : public rclcpp::Node {
 
   rclcpp::TimerBase::SharedPtr heartbeat;
 
-  // TODO: Declare a std::unique_ptr<tf2_ros::TransformBroadcaster>
-  // ...
+  // TODO: Declare a
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
+  // These 3 are member functions - defining some functions that is going to be detailed later
 
  public:
   FramesPublisherNode() : Node("frames_publisher_node") {
     // NOTE: This method is run once, when the node is launched.
 
     // TODO: Instantiate the Transform Broadcaster
+    tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     // ....
 
     startup_time = now();
@@ -41,8 +43,7 @@ class FramesPublisherNode : public rclcpp::Node {
     //   - Get the current time with this->now()
     //   - use the - operator between the current time and startup_time
     //   - convert the resulting Duration to seconds, store result into a double
-
-    // ...
+    double time_t = (this->now() - startup_time).seconds() ;
 
     // Here we declare two geometry_msgs::msg::TransformStamped objects, which
     // need to be populated
@@ -55,6 +56,27 @@ class FramesPublisherNode : public rclcpp::Node {
     //       rotation such that:
     //        i) its y axis stays tangent to the trajectory and
     //       ii) the z vector stays parallel to that of the world frame
+    // -----------------
+    //
+    // Setting yaw = t - - will make the object tangential to the trajectory - ie drone has to rotate to be tangential to circle
+    //
+    //--------------
+    world_T_av1.header.stamp = this->now();
+    world_T_av1.header.frame_id = "world";   // parent frame id
+    world_T_av1.child_frame_id = "av1";       // id of object i am defining the tf for  - how is av1 wrt world - is what is defined here
+    std::vector<float> av1_translation(3);
+    tf2::Quaternion q;
+    double yaw = time_t;
+    q.setRPY(0,0,yaw);
+    av1_translation = {std::cos(time_t),std::sin(time_t),0} ;
+
+    world_T_av1.transform.translation.x = av1_translation[0];
+    world_T_av1.transform.translation.y = av1_translation[1];
+    world_T_av1.transform.translation.z = av1_translation[2];
+    world_T_av1.transform.rotation.x = q.x();
+    world_T_av1.transform.rotation.y = q.y();
+    world_T_av1.transform.rotation.z = q.z();
+    world_T_av1.transform.rotation.w = q.w();
     //
     //     - world_T_av2 shoud have origin in [sin(time), 0.0, cos(2*time)], the
     //       rotation is irrelevant to our purpose.
@@ -62,6 +84,23 @@ class FramesPublisherNode : public rclcpp::Node {
     //    assignment,
     //          make sure you get it right
     //
+    world_T_av2.header.stamp = this->now();
+    world_T_av2.header.frame_id = "world";
+    world_T_av2.child_frame_id = "av2";
+
+    std::vector<float> av2_translation(3);
+    tf2::Quaternion q2;
+
+    av2_translation = {std::sin(time_t),0,std::cos(2*time_t)};
+    q2.setRPY(0,0,0);
+
+    world_T_av2.transform.translation.x = av2_translation[0];
+    world_T_av2.transform.translation.y = av2_translation[1];
+    world_T_av2.transform.translation.z = av2_translation[2];
+    world_T_av2.transform.rotation.x = q2.x();
+    world_T_av2.transform.rotation.y = q2.y();
+    world_T_av2.transform.rotation.z = q2.z();
+    world_T_av2.transform.rotation.w = q2.w();
     //    HINTS:
     //    - check out the ROS tf2 Tutorials:
     //    https://docs.ros.org/en/humble/Tutorials/Intermediate/Tf2/Tf2-Main.html
@@ -76,6 +115,8 @@ class FramesPublisherNode : public rclcpp::Node {
     //         1. you need to define a
     //         std::unique_ptr<tf2_ros::TransformBroadcaster> as a member of the
     //            node class (line 18) and use its sendTransform method below
+    tf_broadcaster->sendTransform(world_T_av1);
+    tf_broadcaster->sendTransform(world_T_av2);
     //         2. the frame names are crucial for the rest of the assignment,
     //            make sure they are as specified, "av1", "av2" and "world"
 
